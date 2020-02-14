16f8b5bc78936b58b577e368b0a8278a4c7d5759
hojunlee77 <54741551+hojunlee77@users.noreply.github.com>
2020-02-14 18:06:10 +0900

    2.11 finished

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,19 @@
 import express from "express";
+import routes from "../routes";
+import {
+  videos,
+  upload,
+  videoDetail,
+  videoEdit,
+  deleteVideo
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.videoEdit, videoEdit);
+videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
