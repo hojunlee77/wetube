78c0ed6d19c442eb2ba4f651726ffdd436bbab92
hojunlee77 <54741551+hojunlee77@users.noreply.github.com>
2020-02-14 10:56:54 +0900

    2019.02.13

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,8 @@
 import express from "express";
+import routes from "../routes";
 
 const globalRouter = express.Router();
+
+globalRouter.get(routes.home, (req, res) => res.send("HOME"));
 
 export default globalRouter;
