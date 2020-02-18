27894058c0be4809a45f1b789c3872bcb09c5eb6
hojunlee77 <54741551+hojunlee77@users.noreply.github.com>
2020-02-18 17:05:25 +0900

    2.21. finished

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    id: 128371,
+    title: "Awesome Video",
+    description: "is this awesome?",
+    views: 23,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 12312312,
+      name: "JUNHO",
+      email: "junho@junho.com"
+    }
+  },
+  {
+    id: 125614,
+    title: "Awesome Video fake 2",
+    description: "is this awesome?",
+    views: 23,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 12312312,
+      name: "JUNHO",
+      email: "junho@junho.com"
+    }
+  },
+  {
+    id: 349867,
+    title: "Awesome Video fake 3",
+    description: "is this awesome?",
+    views: 23,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 12312312,
+      name: "JUNHO",
+      email: "junho@junho.com"
+    }
+  }
+];
