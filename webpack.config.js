491f255cbef311fb4b98bc5f11ebba5075975a61
hojunlee77 <54741551+hojunlee77@users.noreply.github.com>
2020-03-20 11:14:15 +0900

    03.20. finished

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ const config = {
             loader: "postcss-loader",
             options: {
               plugins() {
-                return [autorpefixer({ browsers: "cover 99.5%" })];
+                return [autorpefixer({ overrideBrowserslist: "cover 99.5%" })];
               }
             }
           },
