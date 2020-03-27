ec63872d9d3e3b41135f8e1bf09720d96c0ea8ea
hojunlee77 <54741551+hojunlee77@users.noreply.github.com>
2020-03-28 00:26:23 +0900

    Prestart

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-import "../scss/styles.scss";
-import "./videoPlayer";
-import "./videoRecorder";
-
-const something = async () => {
-  console.log("something");
-};
---
+++ src/passport.js
@@ -0,0 +1,38 @@
+import passport from "passport";
+import GithubStrategy from "passport-github";
+import FacebookStrategy from "passport-facebook";
+import User from "./models/User";
+import {
+  githubLoginCallBack,
+  facebookCallback
+} from "./controllers/userController";
+import routes from "./routes";
+
+passport.use(User.createStrategy());
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GH_ID,
+      clientSecret: process.env.GH_SECRET,
+      callbackURL: `http://localhost:4000${routes.githubCallback}`
+    },
+    githubLoginCallBack
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FB_ID,
+      clientSecret: process.env.FB_SECRET,
+      callbackURL: `https://9a8e08be.ngrok.io${routes.facebookCallback}`,
+      profileFields: ["id", "displayName", "photos", "email"],
+      scope: ["public_profile", "email"]
+    },
+    facebookCallback
+  )
+);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
